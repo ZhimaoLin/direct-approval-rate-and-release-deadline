115000db736f6c2e26e60a67ec85eab2c271f361
Zhimao Lin <zhimao@ualberta.ca>
2019-03-07 15:08:55 -0700

    Update ReadReview.py

--- ReadReview.py
+++ ReadReview.py
@@ -2,35 +2,77 @@ import pandas as pd
 import sys
 
 def main():
-    csv_file_name = sys.argv[1]
-    print("Processing file: " + csv_file_name)
+    csv_file_path = './corp/metadata/'
+    txt_file_path = './corp/discussion/'
 
-    revision_number_without_change = int(sys.argv[2])
-    print("Revision number without change: %d" %revision_number_without_change)
+    repo_list = ['indexing']
 
-    read_file = pd.read_csv(csv_file_name)
+    for repo_name in repo_list:
 
-
-    index = read_file.groupby(['review_number'])['revision_number'].transform(max) == read_file['revision_number']
-
-    groupby_table = read_file[index]
-    final_table = groupby_table[['review_number', 'revision_number', 'status', 'author', 'url']].copy()
-
-    final_table.loc[final_table.status != 'MERGED', 'status'] = 'Rejected'
-    final_table.loc[(final_table.status != 'Rejected') & (final_table.revision_number <= revision_number_without_change), 'status'] = 'DirectlyApprove'
-    final_table.loc[(final_table.status != 'Rejected') & (final_table.revision_number > revision_number_without_change), 'status'] = 'ApproveAfterChange'
-
-    print(final_table[['review_number', 'revision_number', 'status']])
+        revision_number_without_change = 2
 
 
 
+        read_file = pd.read_csv(csv_file_path + repo_name + '.csv')
 
 
-    
+        index = read_file.groupby(['review_number'])['revision_number'].transform(max) == read_file['revision_number']
 
-    
-                
+        groupby_table = read_file[index]
+        review_table = groupby_table[['review_number', 'revision_number', 'status', 'author', 'url']].copy()
 
+        # As for the status is "NEW," at most of the cases, the review is rejected.
+        # In the indexing.csv, there are 95 out of 8316 rows are NEW. Out of 95 rows, there are 37 reviews
+        review_table.loc[review_table.status != 'MERGED', 'status'] = 'Rejected'
+        review_table.loc[(review_table.status != 'Rejected') & (review_table.revision_number <= revision_number_without_change), 'status'] = 'DirectlyApprove'
+        review_table.loc[(review_table.status != 'Rejected') & (review_table.revision_number > revision_number_without_change), 'status'] = 'ApproveAfterChange'
+
+        # print(review_table[['review_number', 'revision_number', 'status']])
+
+        review_table.loc[:, 'start_date'] = pd.Series('xxx', index=review_table.index)
+        review_table.loc[:, 'close_date'] = pd.Series('===', index=review_table.index)
+
+        print(review_table[['review_number', 'start_date', 'close_date']])
+
+
+        for index, row in review_table.iterrows():
+            review_number = str(row['review_number'])
+            revision_number = str(row['revision_number'])
+
+            discussion_file_name_first = review_number + "_rev1_discussion.txt"
+            discussion_file_name_last = review_number + "_rev" + revision_number + "_discussion.txt"
+
+            # Find code review start date
+            f = open(txt_file_path + repo_name + '/' + review_number + '/' + discussion_file_name_first, 'r')
+            lines = f.readlines()
+            start_date = ''
+
+            for l in lines:
+                if 'date: ' in l:
+                    date = l.split()
+                    start_date = date[1] + ' ' + date[2]
+                    break
+            review_table.loc[index, 'start_date'] = start_date
+            f.close()
+
+            # Find code review close date
+            f = open(txt_file_path + repo_name + '/' + review_number + '/' + discussion_file_name_last, 'r')
+            lines = f.readlines()
+            close_date = ''
+
+            for l in lines:
+                if 'date: ' in l:
+                    date = l.split()
+                    close_date = date[1] + ' ' + date[2]
+
+            review_table.loc[index, 'close_date'] = close_date
+
+            f.close()
+
+
+
+
+        print(review_table[['review_number', 'start_date', 'close_date']])
 
 
 
