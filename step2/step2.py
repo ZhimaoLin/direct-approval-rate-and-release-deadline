b0958d8bb425b3e36db5f7d45417df05694f05c5
pshn111 <spei@ualberta.ca>
2019-03-25 15:53:40 -0600

    add final scv

--- step2/step2.py
+++ step2/step2.py
@@ -6,10 +6,10 @@ import os
 # For unauthenticated requests, the rate limit allows for up to 60 requests per hour.
 g = Github('9fc70c72f642a8979bc0788cdd90d3c9f23cd2a2')
 
-repo_full_name_list = ['eclipse/eclipse.platform.ui', 'eclipse/linuxtools', 'eclipse/jgit', 'eclipse/egit', 'couchbase/couchbase-jvm-core', 'couchbase/ns_server', 'couchbase/testrunner', 'couchbase/ep-engine', 'couchbase/indexing', 'couchbase/couchbase-java-client', 'couchbase/spymemcached']
+#repo_full_name_list = ['eclipse/eclipse.platform.ui', 'eclipse/linuxtools', 'eclipse/jgit', 'eclipse/egit', 'couchbase/couchbase-jvm-core', 'couchbase/ns_server', 'couchbase/testrunner', 'couchbase/ep-engine', 'couchbase/indexing', 'couchbase/couchbase-java-client', 'couchbase/spymemcached']
 
 # repo_full_name_list = ['eclipse/jgit', 'eclipse/egit']
-# repo_full_name_list = ['eclipse/egit']
+repo_full_name_list = ['couchbase/couchbase-jvm-core']
 
 try: 	
     os.mkdir('step2_results')
@@ -30,7 +30,7 @@ for repo_full_name in repo_full_name_list:
     release_date_file_name = os.path.join('./step2_results/', repo_name + "_release_date.txt" )
     release_date_file = open(release_date_file_name, "w")
 
-    release_date_file.write('Total number of releases: ' + str(tag_list.totalCount) + '\n')
+    #release_date_file.write('Total number of releases: ' + str(tag_list.totalCount) + '\n')
     
     print('Total number of releases: ' + str(tag_list.totalCount))
     print('==============================')
@@ -41,8 +41,8 @@ for repo_full_name in repo_full_name_list:
         print(t.commit.commit.committer.date)
         print('\n')
 
-        release_date_file.write(str(t.name) + '\n')
-        release_date_file.write(str(t.commit.sha) + '\n')
-        release_date_file.write(str(t.commit.commit.committer.date) + '\n\n')
+        #release_date_file.write(str(t.name) + '\n')
+        #release_date_file.write(str(t.commit.sha) + '\n')
+        release_date_file.write(str(t.commit.commit.committer.date) + '\n')
 
     release_date_file.close()
---
+++ step3/step3.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import sys
+from datetime import datetime
+from datetime import timedelta
+
+def main():
+    csv_file_path = '../step1/'
+    date_file_path = '../step2/step2_results/'
+
+    repo_list = ['indexing', 'couchbase-jvm-core', 'eclipse.platform.ui', 'ep-engine', 'couchbase-java-client', 'testrunner', 'ns_server', 'jgit', 'egit', 'org.eclipse.linuxtools', 'spymemcached']
+    #repo_list = ['couchbase-jvm-core']
+
+    date = []
+    for repo_name in repo_list:
+        print('Processing csv file: ' + repo_name + '_result.csv')
+
+
+        read_file = pd.read_csv(csv_file_path + repo_name + '_result.csv')
+        release_date = open(date_file_path + repo_name + '_release_date.txt', "r")
+
+        # collect release date and store them in a list
+        for line in release_date:
+            today = datetime.strptime(line.split(' ')[0], '%Y-%m-%d').date()
+            previous = today + timedelta(days=-1)
+            #nextDay = previous + timedelta(days=-1)
+            #nextDay1 = nextDay + timedelta(days=-1)
+            #nextDay2 = nextDay1 + timedelta(days=-1)
+            date.append(str(today))
+            date.append(str(previous))
+            #date.append(str(nextDay))
+            #date.append(str(nextDay1))
+            #date.append(str(nextDay2))
+        release_date.close()
+
+        # generate columns for ApproveAfterChange, DirectlyApprove and Rejected
+        #GroupByDate = read_file.groupby(['close_date']).size().to_frame('size')
+
+        GroupByStatus = read_file.groupby(['close_date', 'status']).size()
+
+        result = GroupByStatus.unstack(level=-1).fillna(0)
+
+        # calculate the number for total commit
+        result['TotalCommit'] = result.iloc[:,-3:].sum(axis=1)
+        #result['TotalCommit'] = result['ApproveAfterChange']+result['DirectlyApprove']+result['Rejected']
+
+        # calculate the number for approval rate
+        #result[['ApprovalRate']] = result.iloc[:, 2].div(result.iloc[:, 4])
+        result['ApprovalRate'] = result['DirectlyApprove']/result['TotalCommit']
+
+        
+
+        #DirectlyApprove = read_file.loc[read_file['status'] == 'DirectlyApprove','close_date'].unique().tolist()
+        #ApproveAfterChange = read_file.loc[read_file['status'] == 'ApproveAfterChange','close_date'].unique().tolist()
+        #Rejected = read_file.loc[read_file['status'] == 'Rejected','close_date'].unique().tolist()
+        
+        #DirectlyApproveByDate = read_file[(read_file['status'] == 'DirectlyApprove') & read_file['close_date'].isin(DirectlyApprove)].groupby('close_date').size().to_frame('size')
+        #ApproveAfterChangeByDate = read_file[(read_file['status'] == 'ApproveAfterChange') & read_file['close_date'].isin(DirectlyApprove)].groupby('close_date').size().to_frame('size')
+        #RejectedByDate = read_file[(read_file['status'] == 'Rejected') & read_file['close_date'].isin(DirectlyApprove)].groupby('close_date').size().to_frame('size')
+
+        result.to_csv(repo_name + '_final.csv')
+       
+        final = pd.read_csv('./' + repo_name + '_final.csv')
+
+        # generate a boolean column to define whether the close date close to the commit date
+        final['CloseToReleaseDate'] = 'False'
+        final.loc[final['close_date'].isin(date), 'CloseToReleaseDate'] = 'True'
+
+        final.to_csv(repo_name + '_final.csv')
+
+
+        #print(final)
+
+
+if __name__ == "__main__":
+    main()
