cde82e7b8d9bff18b4554b7de43dc767e8946d31
Zhimao Lin <zhimao@ualberta.ca>
2019-03-26 19:38:25 -0600

    Create step2_3.py

---
+++ step2/step2_3.py
@@ -0,0 +1,34 @@
+from github import Github
+import os
+
+# Create a Github instance
+# For API requests using Basic Authentication or OAuth, you can make up to 5000 requests per hour. 
+# For unauthenticated requests, the rate limit allows for up to 60 requests per hour.
+g = Github('9fc70c72f642a8979bc0788cdd90d3c9f23cd2a2')
+# g = Github()
+
+repo_full_name = 'eclipse/eclipse.platform.ui'
+
+
+repo_name = repo_full_name.split('/')[1]
+
+repo = g.get_repo(repo_full_name)
+tag_list = repo.get_tags()
+print('==============================')
+print('Accessing repo: [' + repo.name + ']')
+
+
+
+release_date_file_name = os.path.join('./step2_results/', repo_name + "_release_date.txt" )
+release_date_file = open(release_date_file_name, "w")
+
+print('Total number of releases: ' + str(tag_list.totalCount))
+print('==============================')
+
+
+total_count = tag_list.totalCount
+
+for t in tag_list[4500:]: 
+    release_date_file.write(str(t.commit.commit.committer.date) + '\n')
+
+release_date_file.close()
