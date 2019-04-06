f16f3d7f36bc627298458c14af1ab5a72d31dee3
Zhimao Lin <zhimao@ualberta.ca>
2019-04-06 13:36:02 -0600

    Update sep3_2.R

--- step3/sep3_2.R
+++ step3/sep3_2.R
@@ -4,7 +4,9 @@ wd <- paste(getwd(),  '/step3_results', sep = "", collapse = NULL)
 setwd(wd)
 getwd()
 
-for (fileName in c('couchbase-java-client_final.csv', 'couchbase-jvm-core_final.csv', 'eclipse.platform.ui_final.csv', 'egit_final.csv', 'ep-engine_final.csv', 'jgit_final.csv', 'linuxtools_final.csv', 'ns_server_final.csv', 'org.eclipse.linuxtools_final.csv', 'spymemcached_final.csv', 'testrunner_final.csv')){
+fileNameList <- c('couchbase-java-client_final.csv', 'couchbase-jvm-core_final.csv', 'eclipse.platform.ui_final.csv', 'egit_final.csv', 'ep-engine_final.csv', 'jgit_final.csv', 'linuxtools_final.csv', 'ns_server_final.csv', 'org.eclipse.linuxtools_final.csv', 'spymemcached_final.csv', 'testrunner_final.csv')
+
+for (fileName in fileNameList){
   print(fileName)
   d <- read.csv(fileName)
   result <- t.test(d$DirectlyApproveRate~d$CloseToReleaseDate)
