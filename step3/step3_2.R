32f374efd9b47238a6d932687bb05c849e098098
Zhimao Lin <zhimao@ualberta.ca>
2019-04-07 00:40:07 -0600

    Update step3_2.R

--- step3/step3_2.R
+++ step3/step3_2.R
@@ -1,24 +1,64 @@
 # Run Rscript step3_2.R
+# https://stats.idre.ucla.edu/r/dae/logit-regression/
+# https://www.statmethods.net/advstats/glm.html
+# http://www-bcf.usc.edu/~gareth/ISL/ISLR%20First%20Printing.pdf
 
 wd <- paste(getwd(),  '/step3_results', sep = "", collapse = NULL)
 setwd(wd)
 getwd()
 
-fileNameList <- c('couchbase-java-client_final.csv', 'couchbase-jvm-core_final.csv', 'eclipse.platform.ui_final.csv', 'egit_final.csv', 'ep-engine_final.csv', 'jgit_final.csv', 'linuxtools_final.csv', 'ns_server_final.csv', 'org.eclipse.linuxtools_final.csv', 'spymemcached_final.csv', 'testrunner_final.csv')
+fileNameList <- c('couchbase-java-client_final.csv', 'couchbase-jvm-core_final.csv', 'eclipse.platform.ui_final.csv', 'egit_final.csv', 'ep-engine_final.csv', 'jgit_final.csv', 'linuxtools_final.csv', 'ns_server_final.csv', 'linuxtools_final.csv', 'spymemcached_final.csv', 'testrunner_final.csv')
+# fileNameList <- c('couchbase-jvm-core_final.csv')
+
+pValueThreshold <- 0.01
+onlyShowSignificant <- TRUE   # Enter TRUE or FALSE
 
 for (fileName in fileNameList){
-  print(fileName)
   d <- read.csv(fileName)
-  result <- t.test(d$DirectlyApproveRate~d$CloseToReleaseDate)
-  # print(result)
+  d$CloseToReleaseDate <- factor(d$CloseToReleaseDate)
+
+  print('=============================================')
+  print(fileName)
+  print('=============================================')
+  print('|     T test                                |')
+  print('=============================================')
 
   # h0: two means are similar 
   # hAlpha: two means are different
   # if p <= Alpha --> Against h0
   # alpha = 0.01 99% confidence 
+  result <- t.test(d$DirectlyApproveRate~d$CloseToReleaseDate)
+  pValue <- result$p.value
 
-  if (result$p.value <= 0.01) {
+  if (onlyShowSignificant == FALSE) {
     print(result)
-    print(result$p.value <= 0.01)
+  } else {
+    if (pValue <= pValueThreshold) {
+      print(result)
+    }
   }
+
+  print('=============================================')
+  print('|     Logistics Regression                  |')
+  print('=============================================')
+
+  # If p-value <= pValueThreshold, 
+  # then there is association between DirectlyApproveRate and CloseToReleaseDate.
+  mylogit <- glm(d$CloseToReleaseDate ~ d$DirectlyApproveRate, data = d, family="binomial")
+  result <- summary(mylogit)
+
+  pValue <- result$coefficients[2,4]
+
+  if (onlyShowSignificant == FALSE) {
+    print(result)
+  } else {
+    if (pValue <= pValueThreshold) {
+      print(result)
+    }
+  }
+
+  print('=============================================')
+  cat('\n')
+  cat('\n')
+  cat('\n')
 }
